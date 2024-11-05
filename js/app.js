b5ed217b0ab64f0dba9b5de4f1b8aef147b53b68
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-11-05 23:32:11 +0000

    Added code to control the carousel movements and button placements

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // hero carosel
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+               
+            },
+            600:{
+               
+            },
+            1000:{
+               
+            }
+        }
+    })
+
+});
