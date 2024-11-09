e41e047fa1cacf3c0d040efc606b632ffe27fa13
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-11-09 01:09:29 +0000

    Added interactivity for services slider

--- js/app.js
+++ js/app.js
@@ -19,4 +19,32 @@ $(document).ready(function() {
         }
     })
 
+
+    // service carosel
+    $('#services-slider').owlCarousel({
+        loop:true,
+        nav:true,
+        smartSpeed: 1000,
+        autoplay:true,
+        autoplayTimeout:2000,
+        margin: 25,
+        dots: false,
+        navText: ['PREV', 'NEXT'],
+        responsive:{
+            0:{
+                items:1,
+                nav: false,
+                margin: 0
+            },
+            768:{
+                items:2
+            },
+            1140:{
+               items:2,
+               center: true,
+               dots: true
+            }
+        }
+    })
+    
 });
